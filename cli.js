14345bd8d810949069d968b45def0374857befc2
p1st1s <emmanueladeboye2017@gamil.com>
2020-11-26 00:00:52 +0100

    - Accept input from the commandline - Arrange result in a table-like structure

--- cli.js
+++ cli.js
@@ -1,6 +1,26 @@
 const assistant = require('./lib/assistant.js');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
 const bob = new assistant.Assistant('Bob');
 
 bob.greet();
-// bob.presentNationalSummaryData();
-//bob.presentFullDetails();
+
+readline.question(
+    `
+        How may i help you today?
+
+        Press 1 to check national summary of Nigeria Covid-19 statistics
+        Press 2 to see statistics for all states.
+        press ctrl + c to exit.
+    `, choice => {
+    
+    if (choice == 1) {
+        bob.presentNationalSummaryData();
+    } else if (choice == 2) {
+        bob.presentFullDetails()
+    }
+    readline.close()
+  })
